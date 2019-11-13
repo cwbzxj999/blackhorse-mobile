1a7af514ba166845a9368a976de2662906a1bbfc
cwbzxj999 <cwb19971015@163.com>
2019-11-13 21:48:45 +0800

    第一天完成

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // 将单位转化为rem
+    'postcss-pxtorem': {
+      // 基准值  iPhone6的尺寸：375px
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入移动端vue组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 自己的样式
+import '@/styles/index.less'
+
+// 导入修改rem基准值的js插件  是在打包后使用
+import 'amfe-flexible'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
